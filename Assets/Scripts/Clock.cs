6125a6481536826d70e4dfb5d54c112d9d6ad5d3
HanenJMS <Hanensgamelab@gmail.com>
2022-11-07 00:07:19 -0500

    Clock seconds continuously moves

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,43 +7,17 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     [SerializeField] Transform hourPivot, minutePivot, secondPivot;
-    DateTime time = DateTime.Now;
     const float hoursToDegrees = -30f, 
                 minutesToDegrees = -6f, 
                 secondsToDegree = -6f;
-    int currentHour, currentMinute, currentSecond;
-    private void Awake()
-    {
-        currentHour = time.Hour;
-        UpdateTime(hourPivot, hoursToDegrees, time.Hour);
-        currentMinute = time.Minute;
-        UpdateTime(minutePivot, minutesToDegrees, time.Minute);
-        currentSecond = time.Second;
-        UpdateTime(secondPivot, secondsToDegree, time.Second);
-    }
     private void Update()
     {
-        time = DateTime.Now;
+        TimeSpan time = DateTime.Now.TimeOfDay;
 
-        if(time.Hour != currentHour)
-        {
-            UpdateTime(hourPivot, hoursToDegrees, time.Hour);
-            currentHour = time.Hour;
-        }
-        else if(time.Minute != currentMinute)
-        {
-            UpdateTime(minutePivot, minutesToDegrees, time.Minute);
-            currentMinute = time.Minute;
-        }
-        else if(time.Second != currentSecond)
-        {
-            UpdateTime(secondPivot, secondsToDegree, time.Second);
-            currentSecond = time.Second;
-        }
-    }
+        hourPivot.localRotation = Quaternion.Euler(0.0f, 0.0f, hoursToDegrees * (float)time.TotalHours);
 
-    private void UpdateTime(Transform transform, float degree, int Time)
-    {
-        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, degree * Time);
+        minutePivot.localRotation = Quaternion.Euler(0.0f, 0.0f, minutesToDegrees * (float)time.TotalMinutes);
+        
+        secondPivot.localRotation = Quaternion.Euler(0.0f, 0.0f, secondsToDegree * (float)time.TotalSeconds);
     }
 }
